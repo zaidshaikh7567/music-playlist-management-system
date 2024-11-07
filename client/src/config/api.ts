69776f0fb99f13db6b10d537d77a41b0d114300f
Zaid Shaikh <zaidshaikh7567@gmail.com>
2024-11-07 21:42:00 +0530

    fix frontend api token issue

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -6,7 +6,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("authToken");
+    const token = localStorage.getItem("token");
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
